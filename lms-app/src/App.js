dd577a4fa081d07c26ffea845d71614f370f5cf7
Me-keer <keerthikumaresh30@gmail.com>
2022-07-29 12:10:57 +0530

    sample

--- lms-app/src/App.js
+++ lms-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>HELLO World</h1>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        h1 > HELL < /h1> <
+        /div>
+    );
 }
 
 export default App;
