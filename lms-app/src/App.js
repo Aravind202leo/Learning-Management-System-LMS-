f5f0b41c3916f91bbf7908c9edd709d27864a889
AkshayaSelvaraj <akshayaselvaraj2310@gmail.com>
2022-08-02 16:38:41 +0530

    navbar

--- lms-app/src/App.js
+++ lms-app/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import {BrowserRouter as Router , Route, Routes } from 'react-router-dom'
+import Navbar from './pages/Navbar'
+import Home from "./pages/home";
+import About from './pages/about'
+import Services from './pages/services'
+import Products from './pages/products'
+import Contactus from './pages/contactus'
+import Signup from './pages/signup'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>HELLO </h1>
-    </div>
+export default function App() {
+  return(
+    <Router>
+      <Navbar />
+      <Routes>
+        
+          <Route  path="/"  component={< Navbar />} />
+          <Route index component={<Home/> } />
+          <Route path="/about" component={<About/> } />
+          <Route path="services" component={<Services/> } />
+          <Route path="products" component={<Products/>} />
+          <Route path="contactus" component={<Contactus/>} />
+          <Route path="signup" component={<Signup/>} />
+          
+      </Routes>
+      
+        
+    </Router>
   );
 }
+ 
 
-export default App;
+
+
+
+
---
+++ lms-app/src/pages/contactus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function contactus() {
+  return(
+   
+      <h1 className='contactus'>Contact us</h1>
+  
+  )
+}
+
+export default contactus
